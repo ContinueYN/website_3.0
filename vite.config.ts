cfa36633f131a64cbf016b14c3eec3e508ab045b
continueyn <your.email@example.com3258664928@qq.com>
2026-07-08 14:51:39 +0800

    配置GitHub Pages部署：添加base路径、切换到hash路由、创建部署工作流

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/website_3.0/',
   plugins: [
     vue(),
     vueDevTools(),
